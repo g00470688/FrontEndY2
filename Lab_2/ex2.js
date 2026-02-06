47b43ffc459517741172c3ecbf85cd7c95dae37a
g00470688 <g00470688@atu.ie>
2026-02-06 13:32:50 +0000

    comments updated

--- Lab_2/ex2.js
+++ Lab_2/ex2.js
@@ -1,27 +1,35 @@
-var taskObject = /** @class */ (function () {
-    function taskObject() {
-        this.tasks = [];
+var taskHandler = /** @class */ (function () {
+    function taskHandler() {
+        this.tasks = []; // array that stores names of strings
     }
-    taskObject.prototype.addTask = function (task) {
+    /*
+       * @param {string} task - task(string) that will be added
+       * @returns {number} length of the tasks array after adding
+    */
+    taskHandler.prototype.addTask = function (task) {
         this.tasks.push(task);
         console.log("succsessfully added ".concat(task));
         return this.tasks.length;
     };
-    taskObject.prototype.listAllTasks = function () {
+    taskHandler.prototype.listAllTasks = function () {
         for (var _i = 0, _a = this.tasks; _i < _a.length; _i++) {
             var t = _a[_i];
             console.log("task ".concat(t));
         }
     };
-    taskObject.prototype.deleteTask = function (task) {
+    /*
+      * @param {string} task - task(string) that will be deleted
+      * @returns {number} length of the tasks array after deletion
+   */
+    taskHandler.prototype.deleteTask = function (task) {
         var i = this.tasks.indexOf(task);
         this.tasks.splice(i);
         console.log("deleted task ".concat(task));
         return this.tasks.length;
     };
-    return taskObject;
+    return taskHandler;
 }());
-var myTasks = new taskObject();
+var myTasks = new taskHandler();
 myTasks.addTask("baba");
 myTasks.addTask("dada");
 myTasks.listAllTasks();
--- Lab_2/ex2.ts
+++ Lab_2/ex2.ts
@@ -1,24 +1,32 @@
-interface taskManager {
+interface taskManager { // interface for the task handler class
     tasks:Array<string>;
     addTask(task:string):number;
     listAllTasks():void;
     deleteTask(task:string):number;
 }
 
-class taskObject implements taskManager{
-    tasks: string[] = [];
+class taskHandler implements taskManager{ // task handler class
+    tasks: string[] = []; // array that stores names of strings
+
+    /*
+       * @param {string} task - task(string) that will be added
+       * @returns {number} length of the tasks array after adding
+    */
     addTask(task: string): number {
         this.tasks.push(task);
         console.log(`succsessfully added ${task}`);
         return this.tasks.length;
     }
 
-    listAllTasks(): void {
+    listAllTasks(): void { // lists all tasks
         for(const t of this.tasks){
             console.log(`task ${t}`);
         }
     }
-
+     /*
+       * @param {string} task - task(string) that will be deleted
+       * @returns {number} length of the tasks array after deletion
+    */
     deleteTask(task: string): number {
         const i = this.tasks.indexOf(task);
         this.tasks.splice(i);
@@ -28,7 +36,7 @@ class taskObject implements taskManager{
 }
 
 
-const myTasks = new taskObject();
+const myTasks = new taskHandler();
 myTasks.addTask("baba");
 myTasks.addTask("dada");
 myTasks.listAllTasks();
